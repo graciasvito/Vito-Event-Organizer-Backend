8bf24acc9cab9de9aa283a7234fb28ffa3f39012
graciasvito <vitoristo1@gmail.com>
2022-12-22 20:33:18 +0700

    remove console

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -80,7 +80,7 @@ module.exports = {
         statusText = "Internal Server Error",
         error: errorData = null,
       } = error;
-      console.log(error);
+
       return wrapper.response(response, status, statusText, errorData);
     }
   },
